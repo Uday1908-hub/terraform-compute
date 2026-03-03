1d9a3fe6f1de74bd97ce5cded3008e8a58930c0b
Uday1908-hub <Uday.Kiran.S@gds.ey.com>
2026-03-03 12:33:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
  cloud {
    organization = "Uday_Kiran"
-   workspaces { name = "compute-workspace" }
+   workspaces { name = "terraform-compute" }
  }
  required_providers {
    azurerm = { source = "hashicorp/azurerm", version = "~> 3.0" }
